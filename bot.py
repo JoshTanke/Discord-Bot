2acf6c4e42c5c4eb3511d7fb7a12c9b53b5f5753
Josh <jtanke@uoregon.edu>
2020-06-07 15:49:07 -0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,160 +1,155 @@
+"""Simple Discord bot for handling Valorant Teams commands.
+"""
 from absl import app
 from absl import flags
+import asyncio
+import json
+import sys
+import time
 import traceback
 from typing import List, Optional, Tuple
-import sys
-import asyncio
-import time
-import json
 
 import discord
 from discord.ext import commands
 from team_generator import TeamGenerator, Rank
 
-print(discord.__file__)
-
 FLAGS = flags.FLAGS
-flags.DEFINE_integer("seed", 10, "Seed to use when generating random teams.")
+flags.DEFINE_integer('seed', 10, 'Seed to use when generating random teams.')
+flags.DEFINE_string('token', None, 'The Client token to use for authorizing the bot.')
+flags.mark_flag_as_required('token')
 
 
 _COMMAND_PREFIX = './'
 
 _RANKS = {
-	'b': Rank.BRONZE,
-	'bronze': Rank.BRONZE,
-	'i': Rank.IRON,
-	'iron': Rank.IRON,
-	's': Rank.SILVER,
-	'silver': Rank.SILVER,
-	'g': Rank.GOLD,
-	'gold': Rank.GOLD,
-	'p': Rank.PLATINUM,
-	'platinum': Rank.PLATINUM,
-	'plat': Rank.PLATINUM,
-	'd': Rank.DIAMOND,
-	'diamond': Rank.DIAMOND,
+  'b': Rank.BRONZE,
+  'bronze': Rank.BRONZE,
+  'i': Rank.IRON,
+  'iron': Rank.IRON,
+  's': Rank.SILVER,
+  'silver': Rank.SILVER,
+  'g': Rank.GOLD,
+  'gold': Rank.GOLD,
+  'p': Rank.PLATINUM,
+  'platinum': Rank.PLATINUM,
+  'plat': Rank.PLATINUM,
+  'd': Rank.DIAMOND,
+  'diamond': Rank.DIAMOND,
 }
 
 
 class ValorantTeamsClient(discord.Client):
 
-	def __init__(self, seed: Optional[int]):
-		super().__init__()
-		self.team_generator = TeamGenerator(random_seed=seed)
-		self.channels = {}
+  def __init__(self, seed: Optional[int]):
+    super().__init__()
+    self.team_generator = TeamGenerator(random_seed=seed)
+    self.channels = {}
 
-	async def on_ready(self):
-		print(f'Logged on as {self.user}!')
+  async def on_ready(self):
+    print(f'Logged on as {self.user}!')
 
-	async def on_message(self, message):
-		author, channel, content = message.author, message.channel, message.content
-		if content.startswith(_COMMAND_PREFIX):
-			command = content.split(_COMMAND_PREFIX)[1]
-			response = await self._process_command(command, author)
-			if response:
-				await channel.send(response)
+  async def on_message(self, message):
+    if message.content.startswith(_COMMAND_PREFIX):
+      command = message.content.split(_COMMAND_PREFIX)[1]
+      response = await self._process_command(command, message.author)
+      await message.channel.send(response)
 
-	async def on_error(event, *args, **kwargs):
-		error_message = sys.exc_info()[1]
-		channel = args[1].channel
-		await channel.send(error_message)
+  async def on_error(event, *args, **kwargs):
+    error_message = sys.exc_info()[1]
+    channel = args[1].channel
+    await channel.send(error_message)
 
-	async def _process_command(self, command: str, author: discord.member.Member) -> Optional[str]:
-		stripped = command.strip()
-		split = [s.lower() for s in command.split(' ') if s]
-		command_type = split[0]
-		args = tuple(arg for arg in split[1:] if not arg.startswith('--'))
-		flags = tuple(f.strip('--') for f in split[1:] if f.startswith('--'))
-		if command_type == 'build':
-			return self._build_player_list_from_channel(author.voice.channel, args, flags)
-		elif command_type == 'list':
-			return self._list(args, flags)
-		elif command_type == 'clear':
-			return self._clear(args, flags)
-		elif command_type == 'add':
-			return self._add(args, flags)
-		elif command_type == 'remove':
-			return self._remove(args, flags)
-		elif command_type == 'generate' or command_type == 'gen':
-			return self._generate(args, flags)
-		# elif command_type == 'listen':
-		# 	return await self._listen(author.voice.channel, args, flags)
-		raise ValueError(f'Unsupported command: {command_type}')
+  async def _process_command(self, command: str, author: discord.member.Member) -> str:
+    stripped = command.strip()
+    split = [s for s in command.split(' ') if s]
+    command_type = split[0]
+    args = tuple(arg for arg in split[1:] if not arg.startswith('--'))
+    flags = tuple(f.strip('--').lower() for f in split[1:] if f.startswith('--'))
+    # TODO(Josh): Create Command abc class for handling command requests. 
+    if command_type == 'add':
+      return self._add(args, flags)
+    elif command_type == 'build':
+      return self._build_player_list_from_channel(author.voice.channel, args, flags)
+    elif command_type == 'clear':
+      return self._clear(args, flags)
+    elif command_type == 'generate' or command_type == 'gen':
+      return self._generate(args, flags)
+    elif command_type == 'list':
+      return self._list(args, flags)
+    # TODO(Josh): Add support for voice commands.   
+    # elif command_type == 'listen':
+    #   return await self._listen(author.voice.channel, args, flags)
+    elif command_type == 'remove':
+      return self._remove(args, flags)
+    raise ValueError(f'Unsupported command: {command_type}')
 
-	def _build_player_list_from_channel(self, channel: discord.channel.VoiceChannel, args: Tuple[str], flags: Tuple[str]):
-		succeeded, failed = [], []
-		for member in channel.members:
-			added = False
-			for role in member.roles:
-				if role.name.lower() in _RANKS:
-					self.team_generator.add_player(member.name, _RANKS[role.name.lower()])
-					added = True
-			if added:
-				succeeded.append(member.name)
-			else:
-				failed.append(member.name)
-		return_string = ''
-		if succeeded:
-			return_string += f'Successfully added: {succeeded}'
-		if failed:
-			return_string += f'\nFailed to add: {failed}'
-		return return_string 
+  def _add(self, args: Tuple[str], flags: Tuple[str]) -> str:
+    if len(args) != 2:
+      return f'Expected 2 arguments, got: {args}'
+    player_name, rank_name = args
+    if rank_name.lower() not in _RANKS:
+      return f'Unsupported rank: {rank_name}'
+    rank = _RANKS[rank_name.lower()]
+    self.team_generator.add_player(player_name, rank)
+    return f'Successfully added {player_name.title()}'
 
-	def _list(self, args: Tuple[str], flags: Tuple[str]):
-		players = self.team_generator.list_players()
-		if players:
-			if 'include_rank' in flags:
-				return ' | '.join([f'{p.name}: {p.rank.name.title()}' for p in players])
-			return ' | '.join([str(p) for p in players]) 
-		return 'Players list is empty.'
+  def _clear(self, args: Tuple[str], flags: Tuple[str]) -> str:
+    self.team_generator.clear_players()
+    return 'Players list cleared.'
 
-	def _clear(self, args: Tuple[str], flags: Tuple[str]):
-		self.team_generator.clear_players()
-		return 'Players list cleared.'
+  def _build_player_list_from_channel(self, channel: discord.channel.VoiceChannel, args: Tuple[str], flags: Tuple[str]) -> str:
+    succeeded, failed = [], []
+    for member in channel.members:
+      added = False
+      for role in member.roles:
+        if role.name.lower() in _RANKS:
+          self.team_generator.add_player(member.name, _RANKS[role.name.lower()])
+          added = True
+      if added:
+        succeeded.append(member.name)
+      else:
+        failed.append(member.name)
+    return_string = ''
+    if succeeded:
+      return_string += f'Successfully added: {succeeded}'
+    if failed:
+      return_string += f'\nFailed to add: {failed}'
+    return return_string 
 
-	def _add(self, args: Tuple[str], flags: Tuple[str]):
-		if len(args) != 2:
-			return f'Expected 2 arguments, got: {args}'
-		player_name, rank_name = args
-		if rank_name not in _RANKS:
-			return f'Unsupported rank: {rank_name}'
-		rank = _RANKS[rank_name]
-		result = self.team_generator.add_player(player_name, rank)
-		if result:
-			return f'Successfully added {player_name.title()}'
-		return f'Failed to add {player_name.title()}'
+  def _generate(self, args: Tuple[str], flags: Tuple[str]) -> str:
+    ignore_length = 'force' in flags
+    teams = self.team_generator.generate(ignore_length=ignore_length)
+    return teams    
 
+  def _list(self, args: Tuple[str], flags: Tuple[str]) -> str:
+    players = self.team_generator.list_players()
+    if players:
+      if 'include_rank' in flags:
+        return ' | '.join([f'{str(p)}: {p.rank.name.title()}' for p in players])
+      return ' | '.join([str(p) for p in players]) 
+    return 'Players list is empty.'
 
-	def _remove(self, args: Tuple[str], flags: Tuple[str]):
-		if len(args) != 1:
-			return f'Expected 1 argument, got: {args}'
-		player_name = args[0]
-		result = self.team_generator.remove_player(player_name)
-		if result:
-			return f'Successfully removed {player_name.title()}'
-		return f'Failed to remove {player_name.title()}'
+  # async def _listen(self, channel: discord.channel.VoiceChannel, args: Tuple[str], flags: Tuple[str]):
+  #   voice_client = await channel.connect()
+  #   t_end = time.time() + 10
+  #   while time.time() < t_end:
+  #     msg = await voice_client.ws.recv()
+  #     print(msg)
+  #     time.sleep(1)
+  #   await voice_client.disconnect()
 
-	def _generate(self, args: Tuple[str], flags: Tuple[str]):
-		check_length = 'force' not in flags
-		teams = self.team_generator.generate(check_length=check_length)
-		return teams
-
-	# async def _listen(self, channel: discord.channel.VoiceChannel, args: Tuple[str], flags: Tuple[str]):
-	# 	voice_client = await channel.connect()
-	# 	t_end = time.time() + 10
-	# 	while time.time() < t_end:
-	# 		msg = await voice_client.ws.recv()
-	# 		print(msg)
-	# 		time.sleep(1)
-	# 	await voice_client.disconnect()
+  def _remove(self, args: Tuple[str], flags: Tuple[str]) -> str:
+    if len(args) != 1:
+      return f'Expected 1 argument, got: {args}'
+    player_name = args[0]
+    self.team_generator.remove_player(player_name)
+    return f'Successfully removed {player_name}'
 
 
 def main(argv):
-	del argv  # Unused.
-	with open('client_secret.json', 'r') as f:
-		token = json.load(f).get('Token')
-	client = ValorantTeamsClient(FLAGS.seed)
-	client.run(token)
+  del argv  # Unused.
+  client = ValorantTeamsClient(FLAGS.seed)
+  client.run(FLAGS.token)
 
 
 if __name__ == '__main__':
--- team_generator.py
+++ team_generator.py
@@ -1,9 +1,10 @@
+"""Classes for generating two weighted randomly selected teams for Valorant scrims. 
+"""
 import enum
 from dataclasses import dataclass
 from typing import List, Optional
 
 import numpy as np
-
 
 
 _TEAM_DISPLAY_TEMPLATE = """
@@ -12,97 +13,102 @@ Team B: {team_b}, Team Score: {team_b_score}
 """
 
 
+# Valorant Ranks.
 class Rank(enum.Enum):
-	IRON = 0.5
-	BRONZE = 1.0
-	SILVER = 1.5
-	GOLD = 2.0
-	PLATINUM = 3.5 
-	DIAMOND = 5.0
+  IRON = 0.5
+  BRONZE = 1.0
+  SILVER = 1.5
+  GOLD = 2.0
+  PLATINUM = 3.5 
+  DIAMOND = 5.0
 
 
 @dataclass
 class Player(object):
-	'''Class for tracking Valorant players.'''
-	name: str
-	rank: Rank
-	
-	def __truediv__(self, other):
-		if isinstance(other, self.__class__):
-			return self.rank.value / other.rank.value
-		elif isinstance(other, int) or isinstance(other, float):
-			return self.rank.value / other
-		raise TypeError(f'Unsupported type for division: {type(other)}')
+  '''Class for tracking Valorant players.'''
+  name: str
+  rank: Rank
+  
+  def __truediv__(self, other):
+    if isinstance(other, self.__class__):
+      return self.rank.value / other.rank.value
+    elif isinstance(other, int) or isinstance(other, float):
+      return self.rank.value / other
+    raise TypeError(f'Unsupported type for division: {type(other)}')
 
-	def __eq__(self, other):
-		if isinstance(other, self.__class__):
-			return self.name.lower() == other.name.lower()
-		return False
+  def __eq__(self, other):
+    if isinstance(other, self.__class__):
+      return self.name.lower() == other.name.lower()
+    return False
 
-	def __lt__(self, other):
-		if self.rank.value == other.rank.value:
-			return self.name < other.name
-		return self.rank.value < other.rank.value	
+  def __lt__(self, other):
+    if self.rank.value == other.rank.value:
+      return self.name < other.name
+    return self.rank.value < other.rank.value 
 
-	def __repr__(self):
-		return self.name
+  def __repr__(self):
+    return self.name
+
 
 class TeamGenerator(object):
+  '''Class for generating two weighted random Valorant teams.
+  
+  Players are randomly selected from a List[Player], which the rank of the
+  Player increasing their odds of getting selected. 
+  '''
 
-	def __init__(self, random_seed: Optional[int] = None):
-		if random_seed: np.random.seed(random_seed)
-		self.players = []
+  def __init__(self, random_seed: Optional[int] = None):
+    if random_seed: np.random.seed(random_seed)
+    self.players = []
 
-	def generate(self, check_length: bool = True) -> str:
-		if check_length and len(self.players) != 10:
-			raise ValueError(f'Can only match 10 players. Currently have {len(self.players)}.')
-		if len(self.players) % 2 != 0:
-			raise ValueError(f'Must have an even number of players. Currently have {len(self.players)}.')
+  def generate(self, ignore_length: bool = False) -> str:
+    if not ignore_length and len(self.players) != 10:
+      raise ValueError(f'Can only match 10 players. Currently have {len(self.players)}.')
+    if len(self.players) % 2 != 0:
+      raise ValueError(f'Must have an even number of players. Currently have {len(self.players)}.')
 
-		temp_players = self.players.copy()
-		team_a, team_b = [], []
-		while temp_players:
-			team_a.append(self._poll(temp_players))
-			team_b.append(self._poll(temp_players))
-			team_a = sorted(team_a, reverse=True)
-			team_b = sorted(team_b, reverse=True)
-		team_a_score = self._evaluate_team(team_a)
-		team_b_score = self._evaluate_team(team_b)
-		return _TEAM_DISPLAY_TEMPLATE.format(
-			team_a=team_a,
-			team_a_score=team_a_score,
-			team_b_score=team_b_score,
-			team_b=team_b)
+    temp_players = self.players.copy()
+    team_a, team_b = [], []
+    while temp_players:
+      team_a.append(self._poll(temp_players))
+      team_b.append(self._poll(temp_players))
+      team_a = sorted(team_a, reverse=True)
+      team_b = sorted(team_b, reverse=True)
 
-	def add_player(self, player_name: str, rank: Rank) -> bool:
-		if len(self.players) == 10:
-			raise ValueError(f'Players list is full.')
-			return False
-		player = Player(player_name, rank)
-		if player in self.players:
-			raise ValueError(f'{player_name.title()} is already added.')
-			return False
-		self.players.append(player)
-		return True
+    team_a_score = self._evaluate_team(team_a)
+    team_b_score = self._evaluate_team(team_b)
+    return _TEAM_DISPLAY_TEMPLATE.format(
+      team_a=team_a,
+      team_a_score=team_a_score,
+      team_b_score=team_b_score,
+      team_b=team_b)
 
-	def remove_player(self, player_name: str) -> bool:
-		for p in self.players:
-			if p.name.lower() == player_name.lower():
-				self.players.remove(p)
-				return True
-		return False
+  def add_player(self, player_name: str, rank: Rank):
+    if len(self.players) == 10:
+      raise ValueError(f'Players list is full.')
+    player = Player(player_name, rank)
+    if player in self.players:
+      raise ValueError(f'{player_name} is already added.')
+    self.players.append(player)
 
-	def list_players(self) -> List[Player]:
-		return sorted(self.players, reverse=True)
+  def remove_player(self, player_name: str) -> bool:
+    for p in self.players:
+      if p.name.lower() == player_name.lower():
+        self.players.remove(p)
+        return True
+    return False
 
-	def clear_players(self):
-		self.players = []
+  def list_players(self) -> List[Player]:
+    return sorted(self.players, reverse=True)
 
-	def _poll(self, players: List[Player]) -> Player:
-		total = sum([p.rank.value for p in players])
-		probs = list(np.array(players) / total)
-		index = np.random.choice(range(len(players)), size=1, replace=False, p=probs)[0]
-		return players.pop(index)
+  def clear_players(self):
+    self.players = []
 
-	def _evaluate_team(self, team: List[Player]) -> float:
-		return np.round(sum([p.rank.value for p in team]), decimals=2)
+  def _poll(self, players: List[Player]) -> Player:
+    total = sum([p.rank.value for p in players])
+    probs = list(np.array(players) / total)
+    index = np.random.choice(range(len(players)), size=1, replace=False, p=probs)[0]
+    return players.pop(index)
+
+  def _evaluate_team(self, team: List[Player]) -> float:
+    return np.round(sum([p.rank.value for p in team]), decimals=2)
